4e8a9062229b61388725dbe60eddf3d9c8222ee7
koosco <rnxogud123@naver.com>
2024-04-04 14:50:02 +0900

    initial commit

---
+++ src/main/java/io/security/krampoline_project/KrampolineProjectApplication.java
@@ -0,0 +1,13 @@
+package io.security.krampoline_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrampolineProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrampolineProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/security/krampoline_project/KrampolineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.krampoline_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrampolineProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
